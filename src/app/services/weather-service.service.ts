7fa07c6b2612c4f9b11cf407c1d78c502fe1f0f4
Azar Ansar <azarmohamed96@yahoo.com>
2023-08-22 15:38:49 -0500

    updated for prod

--- src/app/services/weather-service.service.ts
+++ src/app/services/weather-service.service.ts
@@ -1,8 +1,8 @@
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { environment } from 'src/environments/environment';
 import { WeatherModel } from '../model/wetherModel';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.prod';
 
 @Injectable({
   providedIn: 'root'
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  weatherApiBaseURL: 'http://api.weatherapi.com/v1/current.json?key=6b8b1b6f97cd4d2fbbc191928232208',
 };
